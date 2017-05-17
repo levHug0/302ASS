e119d1b81f4355e612caf9c95cd45515ea7af4b6
Levi <levinard.hugo@connect.qut.edu.au>
2017-05-17 16:40:13 +1000

    testing

--- asgn2/src/asgn2Tests/PizzaTests.java
+++ asgn2/src/asgn2Tests/PizzaTests.java
@@ -1,5 +1,15 @@
 package asgn2Tests;
 
+import static org.junit.Assert.*;
+
+import java.time.LocalTime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import asgn2Exceptions.PizzaException;
+import asgn2Pizzas.*;
+
 /**
  * A class that that tests the asgn2Pizzas.MargheritaPizza, asgn2Pizzas.VegetarianPizza, asgn2Pizzas.MeatLoversPizza classes. 
  * Note that an instance of asgn2Pizzas.MeatLoversPizza should be used to test the functionality of the 
@@ -10,4 +20,23 @@ package asgn2Tests;
  */
 public class PizzaTests {
 	// TO DO
+	
+	/* Margerita
+	 * 			Tests
+	 * 					Below
+	 */
+	private Pizza myPizza;
+	
+	@Before
+	public void createMargherita() throws PizzaException {
+		LocalTime time01 = LocalTime.parse("20:30");
+		LocalTime time02 = LocalTime.parse("20:45");
+		myPizza = new MargheritaPizza(3, time01, time02);
+	}
+	
+	@Test
+	public void testMe() throws PizzaException {
+		assertEquals(myPizza.getQuantity(), 3);
+	}
+	
 }
